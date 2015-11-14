1a49fe2f09b80aaf1ffb8536055d974480a2ee6c
mike <mikecaines@gmail.com>
2015-11-13 21:16:58 -0400

    StdoutView: initial revision

---
+++ src/Lightship/StdoutView.php
@@ -0,0 +1,30 @@
+<?php
+namespace Lightship;
+
+use App\Environment as Env;
+use Batten\StandardOutputEvent;
+
+class StdoutView extends View {
+	public function handleStandardOutput(StandardOutputEvent $aEvt) {
+		$this->out($aEvt->getText());
+	}
+
+	public function out($aMessage) {
+		fwrite(STDOUT, $aMessage . "\n");
+	}
+
+	public function render() {
+		//do nothing
+	}
+
+	public function init() {
+		parent::init();
+
+		Env::getStandardOutput()->addEventListener('standard-output', [$this, 'handleStandardOutput']);
+	}
+
+	public function __construct($aCode) {
+		$this->type = 'Stdout';
+		parent::__construct($aCode);
+	}
+}
