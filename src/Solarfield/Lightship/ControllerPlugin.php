7b8f919c0fb3f9d68a65c97433ae25ed06952b41
mike <mikecaines@gmail.com>
2016-04-23 15:01:25 -0300

    Give ControllerPlugin's a manifest, which is higher level config info that is specified in the constructor

--- src/Solarfield/Lightship/ControllerPlugin.php
+++ src/Solarfield/Lightship/ControllerPlugin.php
@@ -1,6 +1,16 @@
 <?php
 namespace Solarfield\Lightship;
 
-class ControllerPlugin extends \Solarfield\Batten\ControllerPlugin {
+use Solarfield\Ok\StructProxy;
 
+class ControllerPlugin extends \Solarfield\Batten\ControllerPlugin {
+	private $manifest;
+
+	public function getManifest() {
+		if (!$this->manifest) {
+			$this->manifest = new StructProxy();
+		}
+
+		return $this->manifest;
+	}
 }
