216e22f175cdb4dbcfa59467ac0e164f7bae980e
mike <mikecaines@gmail.com>
2016-10-08 18:38:40 -0300

    add a way to set/get a custom http status message

--- src/Solarfield/Lightship/HttpException.php
+++ src/Solarfield/Lightship/HttpException.php
@@ -3,18 +3,24 @@ namespace Solarfield\Lightship;
 
 class HttpException extends \Exception implements HttpExceptionInterface {
 	private $httpCode;
+	private $httpMessage;
 
 	public function getHttpStatusCode() {
 		return $this->httpCode;
 	}
 
-	public function __construct($aMessage, $aCode, $aPrevious, $aHttpCode) {
+	public function getHttpStatusMessage() {
+		return $this->httpMessage;
+	}
+
+	public function __construct($aMessage, $aCode, $aPrevious, $aHttpStatusCode, $aHttpStatusMessage = null) {
 		parent::__construct(
-			$aMessage != null ? $aMessage : 'HTTP ' . $aHttpCode,
+			$aMessage != null ? $aMessage : 'HTTP ' . $aHttpStatusCode,
 			$aCode,
 			$aPrevious
 		);
 
-		$this->httpCode = (int)$aHttpCode;
+		$this->httpCode = (int)$aHttpStatusCode;
+		$this->httpMessage = (string)$aHttpStatusMessage;
 	}
 }
--- src/Solarfield/Lightship/HttpExceptionInterface.php
+++ src/Solarfield/Lightship/HttpExceptionInterface.php
@@ -6,4 +6,9 @@ interface HttpExceptionInterface {
 	 * @return int
 	 */
 	public function getHttpStatusCode();
+
+	/**
+	 * @return string|null
+	 */
+	public function getHttpStatusMessage();
 }
