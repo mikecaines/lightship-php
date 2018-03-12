dfc1caf73e7ec442859e3923cd15d29bc0b82582
mikecaines <mikecaines@gmail.com>
2018-03-11 21:36:13 -0300

    comments for StandardOutput

--- src/Solarfield/Lightship/StandardOutput.php
+++ src/Solarfield/Lightship/StandardOutput.php
@@ -5,6 +5,13 @@ use Psr\Log\AbstractLogger;
 use Solarfield\Ok\EventTargetInterface;
 use Solarfield\Ok\EventTargetTrait;
 
+/**
+ * A basic sink for ephemeral messages, implementing LoggerInterface and EventTargetInterface.
+ * Components may listen for the standard-output event, and deal with the messages in some way.
+ * The StdoutView used by the terminal, outputs these messages to the user.
+ * The Html and Json views will store these in the model at app.standardOutput.messages, and they will be output to
+ * the browser console on the client side.
+ */
 class StandardOutput extends AbstractLogger implements EventTargetInterface {
 	use EventTargetTrait;
 	
