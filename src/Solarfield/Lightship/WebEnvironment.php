45ac6dc362da3b559f81d141a35734480cc8bc0a
Mike <mikecaines@gmail.com>
2019-10-27 11:08:32 -0300

    move all "pending data" behaviour to lightship-bridge

--- src/Solarfield/Lightship/WebEnvironment.php
+++ src/Solarfield/Lightship/WebEnvironment.php
@@ -4,12 +4,6 @@ namespace Solarfield\Lightship;
 use Throwable;
 
 abstract class WebEnvironment extends Environment {
-	private $stdoutMessages = [];
-
-	public function takeBufferedStdoutMessages() : array {
-		return array_splice($this->stdoutMessages, 0);
-	}
-
 	public function bail(Throwable $aEx) : DestinationContextInterface {
 		$this->getLogger()->error("Bailed.", ['exception'=>$aEx]);
 		return new WebDestinationContext(500);
@@ -26,19 +20,5 @@ abstract class WebEnvironment extends Environment {
 
 		//url to app dependencies dir, i.e. /__/deps
 		$options->add('appDependenciesWebPath', $options->get('appPackageWebPath') . '/deps');
-
-		//TODO: move this to a LightshipBridge plugin
-		$this->getStandardOutput()->addEventListener('standard-output', function (StandardOutputEvent $aEvt) {
-			// buffer the message
-			$this->stdoutMessages[] = [
-				'message' => $aEvt->getText(),
-				'level' => $aEvt->getLevel(),
-				'context' => $aEvt->getContext(),
-			];
-
-			if ($this->isDevModeEnabled()) {
-				$this->getLogger()->debug('[stdout] [' . $aEvt->getLevel() . '] ' . $aEvt->getText(), $aEvt->getContext());
-			}
-		});
 	}
 }
